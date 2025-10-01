e28ad5d9abaf5b85fbf5e2a227e91b8fc3d30f9d
syuh23 <seonghee5070@gmail.com>
2025-10-01 17:48:30 +0900

    1010, 1149, 1699 - success

--- src/Dynamic_Programming/Q1010.java
+++ src/Dynamic_Programming/Q1010.java
@@ -3,52 +3,29 @@ package Dynamic_Programming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Q1010 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
 
         for (int repeat = 0; repeat < T; repeat++) {
-            String line = br.readLine();
-            int N = Integer.parseInt(line.split(" ")[0]);
-            int M = Integer.parseInt(line.split(" ")[1]);
-            int sum = 0;
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
-            int[][] result = new int[N + 1][M - N + 1];
-            result[N + 1][M - N + 1] = 1;
+            long mulM = 1, mulN = 1;
+            for (int i = 1; i <= N; i++) {
+                mulN *= i;
+                mulM *= (M - i + 1);
+            }
 
-//            ArrayList<Integer> lastValue = new ArrayList<>(2);
-//            lastValue.add(1, M - N + 1);
-//            result.addLast(lastValue);
-//
-//            ArrayList<Integer> secondLastValue = new ArrayList<>(M - N + 1);
-//            secondLastValue.add(0);
-//            for (int i = 1; i <= M - N; i++) {
-//                secondLastValue.add(secondLastValue.get(i - 1) + i);
-//            }
-//            result.add(N - 1, secondLastValue);
-//
-//            for (int i = N - 2; i > 0; i--) {
-//                ArrayList<Integer> newValue = new ArrayList<>(M - N + 1);
-//                newValue.add(1, 1);
-//                for (int j = 2; j <= M - N; j++) {
-//                    newValue.add(j, newValue.get(j - 1) + result.get(i - 1).get(j));
-//                }
-//            }
-//
-//            for (int i = 1; i <= N; i++) {
-//                int size = result.get(i).size();
-//
-//                for (int j = 1; j <= size; j++) {
-//                    sum += result.get(i).get(j);
-//                }
-//            }
-
-            System.out.println(sum);
+            long result = mulM / mulN;
+            System.out.println(result);
         }
     }
+
 }
---
+++ src/Dynamic_Programming/Q1149.java
@@ -0,0 +1,30 @@
+package Dynamic_Programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1149 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][3];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + r;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + g;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + b;
+        }
+
+        System.out.println(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2])));
+    }
+}
